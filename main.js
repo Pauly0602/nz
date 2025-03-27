1f13d38254d4f679067f06072cff28dc50f23d1d
pauly0602 <paulina.woernhoer@student.uibk.ac.at>
2025-03-27 15:50:05 +0100

    variable für let eingegeben (lat,lng,zoom)

--- main.js
+++ main.js
@@ -2,13 +2,17 @@
 Skript für Neuseelandreise 
 */
 // Karte initialisieren
-let map = L.map('map').setView([-41.293056, 174.001944], 11);
+let lat = -41.293056
+let lng = 174.001944
+let zoom = 11
+
+let map = L.map('map').setView([lat, lng], zoom);
 // Hintergrund definieren 
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 // Marker zeichnen
-let marker = L.marker([-41.293056, 174.001944]).addTo(map);
+let marker = L.marker([lat, lng]).addTo(map);
 // Popup definieren und öffnen
 marker.bindPopup("<b>Welcome to Picton!</b><br> In the Marlborough Region of New Zealand's South Island").openPopup();
