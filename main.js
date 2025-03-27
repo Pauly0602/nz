8c59bd837881a383bec3ecf52f31d3aee646d6e1
pauly0602 <paulina.woernhoer@student.uibk.ac.at>
2025-03-27 15:36:09 +0100

    Kommentare ergänzt

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+/*
+Skript für Neuseelandreise 
+*/
+// Karte initialisieren
 var map = L.map('map').setView([-41.293056, 174.001944], 11);
+// Hintergrund definieren 
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+// Marker zeichnen
 var marker = L.marker([-41.293056, 174.001944]).addTo(map);
+// Popup definieren und öffnen
 marker.bindPopup("<b>Welcome to Picton!</b><br> In the Marlborough Region of New Zealand's South Island").openPopup();
