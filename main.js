fc84dee36a15ab17f792efda040387c8c63c184e
pauly0602 <paulina.woernhoer@student.uibk.ac.at>
2025-03-27 18:42:05 +0100

    Änderungen auf Pull-down reagieren

--- main.js
+++ main.js
@@ -220,21 +220,27 @@ let marker = L.marker([STOPS [i].lat,STOPS [i].lng]).addTo(map)
   </ul>
     In the Marlborough Region of New Zealand's South Island.
     `);
-    
+
     // auf eigene Etappe blicken und Popup öffnen 
 
     if (STOPS [i].user == "pauly0602"){
       map.setView([STOPS [i].lat, STOPS [i].lng], STOPS [i].zoom);
       marker.openPopup();
     }
+
+    // Pulldown Menü befüllen
+    let option = document.createElement ("option");
+    option.value = STOPS[i].user;
+    option.text = STOPS[i].title
+    if (STOPS [i].user == "pauly0602") {
+      option.selected = true;
+    }
+    document.querySelector("#pulldown select").appendChild (option);
   }
-
-
-
-
-
- 
-  
-
-
-  
+// auf Änderungen beim Pull-down reagieren 
+document.querySelector ('#pulldown select').onchange  = function (evt) {
+  let url = `https://${evt.target.value }.github.io/nz`
+  //console.log (evt.target.value)
+  //console.log (url)
+  window.location = url;
+}
